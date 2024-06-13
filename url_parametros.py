ce68b9866b0cfd29d9a06c6f3a74761d7571e8ff
Edson Bruno <135662753+sucloudflare@users.noreply.github.com>
2024-06-13 11:43:15 -0300

    Add files via upload

---
+++ url_parametros.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+
+url = 'https://www.lp.nemu.com.br/?utm_source=fb&utm_campaign=adset01|123&utm_medium=campanha01|1234&utm_content=ad|1234'
+
+partes = urlparse(url)
+parametros = parse_qs(partes.query)
+
+base = 'https://suasaudemental.com.br/'
+nova = urlparse(base)
+
+query_string = urlencode(parametros, doseq=True)
+nova_url = urlunparse((
+    nova.scheme,
+    nova.netloc,
+    nova.path,
+    nova.params,
+    query_string,
+    nova.fragment
+))
+
+print(nova_url)
+
